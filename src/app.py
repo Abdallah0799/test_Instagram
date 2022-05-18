0004c1fce7608938ee9a58443654856a5f4239c2
Abdallah0799 <tornadogazal@gmail.com>
2022-05-18 09:39:35 +0200

    modif

--- src/app.py
+++ src/app.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 
 base_url = 'https://www.instagram.com/'
 
-data = pd.read_csv("final_database.csv")
+data = pd.read_csv("final_database.csv", index_col=0)
 
 @app.route('/')
 def index():
@@ -48,16 +48,24 @@ def predict():
     #f = open('test2.json')
     #doc = json.load(f)
     
-    titre_et_bio, description, followers = getInfosFromJson(doc)
-    d_tb, d_p, d_f = getDistances(titre_et_bio, description, followers)
-    names, scores = get_Names_And_Scores(n,d_tb, d_p, d_f)
+    titre_et_bio, description, followers, url_profile_pic = getInfosFromJson(doc)
+    d_tb, d_p, d_f, key_words_bio_et_titre, key_words_posts = getDistances(titre_et_bio, description, followers)
+    names, scores, scores_bio, scores_posts, followers_others, bios, key_words_bt, key_words_p = get_Names_And_Scores(n,d_tb, d_p, d_f)
     
     objects = []
-    for name, score in zip(names, scores):
-        objects.append([name, score])
+    objects2 = []
+   
+    for name, score, s_b, s_p, fol, b, k_bt, k_p in zip(names, scores, scores_bio, scores_posts, followers_others, bios, key_words_bt, key_words_p):
+        objects2.append([name, round(score,3), k_bt, k_p, round(s_b, 3), round(s_p,3), b, fol])
+        
+    
+    objects = [[username, titre_et_bio, followers, key_words_bio_et_titre, key_words_posts]]
+    
+    headers = ['Username', 'Biography', 'Number of followers', 'Key words in Bio', 'Key words in posts']
+    headers2 = ["Usernames","Similarity score", 'Key words in Bio', 'Key words in posts', "Similarity score on biography (intermediate score with a weight of 50%)", "Similarity score on posts (intermediate score with a weight of 50%)", "Biography", "Number of followers"]
     
     
-    return render_template('result.html', headers=["Username", "Similarity score"], objects = objects)
+    return render_template('result.html', username=username, n=n, usernames = names, headers=headers, headers2 =headers2, objects = objects, objects2 = objects2)
 
 
 def getInfosFromJson(doc):
@@ -85,12 +93,17 @@ def getInfosFromJson(doc):
     try:
         followers = doc['graphql']['user']['edge_followed_by']['count']
     except:
-        followers = None
+        followers = ""
+    
+    try:
+        url_profile_pic = doc['graphql']['user']['profile_pic_url']
+    except:
+        url_profile_pic = None
     
     description = ','.join(description)
     titre_et_bio = bio + ' / ' + titre
     
-    return titre_et_bio, description, followers
+    return titre_et_bio, description, followers, url_profile_pic
 
 def getDistances(titre_et_bio, posts_description, followers):
     
@@ -105,6 +118,14 @@ def getDistances(titre_et_bio, posts_description, followers):
     input_vectorizer_bt = vectorizer_bio_et_titre.transform(X_bt)
     M = tfid_bio_et_titre.transform(input_vectorizer_bt)
     vector_to_compare = vectorizer_bio_et_titre.transform([titre_et_bio])
+    inverse_transformation = vectorizer_bio_et_titre.inverse_transform(vector_to_compare)
+    if len(inverse_transformation) > 0:
+        key_words_bio_et_titre = inverse_transformation[0]
+        key_words_bio_et_titre = list(key_words_bio_et_titre)
+        key_words_bio_et_titre = ','.join(sorted(key_words_bio_et_titre))
+    else:
+        key_words_bio_et_titre = "No relevant keywords found"
+    
     vector_to_compare__tfid = tfid_bio_et_titre.transform(vector_to_compare)
     
     null_vector = np.zeros(vector_to_compare__tfid.toarray().shape)
@@ -123,6 +144,14 @@ def getDistances(titre_et_bio, posts_description, followers):
     input_vectorizer_p = vectorizer_posts.transform(X_p)
     M = tfid_posts.transform(input_vectorizer_p)
     vector_to_compare = vectorizer_posts.transform([posts_description])
+    inverse_transformation = vectorizer_posts.inverse_transform(vector_to_compare)
+    if len(inverse_transformation)>0:
+        key_words_post = inverse_transformation[0]
+        key_words_post = list(key_words_post)
+        key_words_post = ','.join(sorted(key_words_post))
+    else:
+        key_words_post = "No relevant key words found"
+    
     vector_to_compare__tfid = tfid_posts.transform(vector_to_compare)
     
     null_vector = np.zeros(vector_to_compare__tfid.toarray().shape)
@@ -145,21 +174,46 @@ def getDistances(titre_et_bio, posts_description, followers):
     
     normalized_distances_f = (distances_f - np.min(distances_f))/np.max(distances_f)
     
-    return normalized_distances_tb, normalized_distances_p, normalized_distances_f
+    return normalized_distances_tb, normalized_distances_p, normalized_distances_f, key_words_bio_et_titre, key_words_post
 
 def get_Names_And_Scores(n, d_tb, d_p, d_f):
     
-    scores = (1 - d_tb) * 0.5 + (1 - d_p) * 0.5
+    scores_bio = (1 - d_tb)
+    scores_posts = (1 - d_p)
+    
+    scores = scores_bio*0.5 + scores_posts*0.5
     
     idx = np.flipud(np.argsort(scores)[-n:])
     scores = np.flipud(np.sort(scores)[-n:])
     
     names = []
+    list_scores_bio = []
+    list_scores_posts = []
+    followers = []
+    bios = []
+    key_words_bt = []
+    key_words_p = []
     for i in idx:
-        name = data.iloc[i][3]
+        name = data.iloc[i][0]
         names.append(name)
+        list_scores_bio.append(scores_bio[i])
+        list_scores_posts.append(scores_posts[i])
+        followers.append(data.iloc[i][2])
+        bios.append(data.iloc[i][8])
+        
+        bt_el = data.iloc[i][10]
+        if type(bt_el) == str:
+            key_words_bt.append(bt_el)
+        else:
+            key_words_bt.append('No relevant keywords found')
+        
+        p_el = data.iloc[i][11]
+        if type(p_el) == str:
+            key_words_p.append(p_el)
+        else:
+            key_words_p.append('No relevant keywords found')
     
-    return names, scores
+    return names, scores, list_scores_bio, list_scores_posts, followers, bios, key_words_bt, key_words_p
     
     
     
